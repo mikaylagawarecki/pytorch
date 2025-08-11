0c1814ddb6e976fe6f91f97c83376799285b0b76
Mikayla Gawarecki <mikaylagawarecki@gmail.com>
2025-08-11 12:15:29 -0700

    Add beginning of torch::stable::accelerator
    ghstack-source-id: 02220eb90777b7cdbd66bc2954d5e933227328bc
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/159679

--- test/cpp_extensions/libtorch_agnostic_extension/test/test_libtorch_agnostic.py
+++ test/cpp_extensions/libtorch_agnostic_extension/test/test_libtorch_agnostic.py
@@ -5,6 +5,7 @@ from pathlib import Path
 
 import torch
 from torch.testing._internal.common_device_type import (
+    deviceCountAtLeast,
     instantiate_device_type_tests,
     onlyCPU,
     onlyCUDA,
@@ -266,6 +267,38 @@ if not IS_WINDOWS:
                 self.assertEqual(out, ref_out, exact_device=True)
             finally:
                 torch.use_deterministic_algorithms(deterministic)
+        
+        @onlyCUDA
+        @deviceCountAtLeast(2)
+        def test_device_guard(self, device):
+            import libtorch_agnostic
+
+            device_index = 1
+            out = libtorch_agnostic.ops.test_device_guard(device_index)
+            self.assertEqual(out, device_index)
+
+        @onlyCUDA
+        @deviceCountAtLeast(2)
+        def test_device_guard_set_index(self, device):
+            import libtorch_agnostic
+
+            # This test creates a DeviceGuard with index 1, then sets it to index 0
+            # and returns the current device (should be 0)
+            out = libtorch_agnostic.ops.test_device_guard_set_index()
+            self.assertEqual(out, 0)
+
+        @onlyCUDA
+        def test_stream(self, device):
+            import libtorch_agnostic
+
+            stream = torch.cuda.Stream()
+            device = torch.cuda.current_device()
+
+            with stream:
+                expected_stream_id = torch.cuda.current_stream(0).stream_id
+                stream_id = libtorch_agnostic.ops.test_stream(device)
+
+            self.assertEqual(stream_id, expected_stream_id)
 
     instantiate_device_type_tests(TestLibtorchAgnostic, globals(), except_for=None)
 
